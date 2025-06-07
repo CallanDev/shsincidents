2bcac09bef5db5bc678af6e5b080d9a8e6c189ad
CallanDev <158743629+CallanDev@users.noreply.github.com>
2025-06-08 00:20:28 +0100

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7xEsYy0IClWdcWc3aYHX_CGU2uoL4inw",
+  authDomain: "shs-incidents.firebaseapp.com",
+  projectId: "shs-incidents",
+  storageBucket: "shs-incidents.firebasestorage.app",
+  messagingSenderId: "525936587552",
+  appId: "1:525936587552:web:436e95dcf29d8eea5a5d45"
+};
+firebase.initializeApp(firebaseConfig);
